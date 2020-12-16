8dac0565297a806ed06010145ac34c220fe8d07b
Pavel Ponomarev <rocksquare@gmail.com>
2020-12-16 21:34:23 +0300

    docker returned

--- customer/src/main/java/customer/config/RabbitMQConfig.java
+++ customer/src/main/java/customer/config/RabbitMQConfig.java
@@ -22,9 +22,9 @@ public class RabbitMQConfig {
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory =
-                new CachingConnectionFactory("localhost");
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
+                new CachingConnectionFactory("host.docker.internal");
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("admin");
         return connectionFactory;
     }
 
